7dcfeb617e37d52b7497b813dda5fc2c00e2697e
andreykuluev96 <andreykuluev96@omo.systems>
2020-05-27 17:15:30 +0300

    1

--- mqtt2gpio.py
+++ mqtt2gpio.py
@@ -6,14 +6,14 @@ from mqtt import *
 client = MQTT("192.168.0.104", 1883, "omo-sl-embed-ua-kiev-andrey", "omo", "hubEvents/omo-sl-embed-ua-kiev-andrey", "gw/7777777777777777/commands")
 client.create_connection() 
 
-led = LED(21) # gpio 21
+#led = LED(21) # gpio 21
 
 while True:
-    led.on()
-    sleep(0.000500-0.000100)
-    led.off()
-    sleep(0.000500-0.000100)
-    #pass
+    #led.on()
+    #sleep(0.000500-0.000100)
+    #led.off()
+    #sleep(0.000500-0.000100)
+    pass
 
 #0.000261 261 мкс
 #0.000001 1 мкс - 0.089 мс
